32b9878c51f9bb0c27d86f6d1989fac3659a6a3e
mattcroat <mattcroat.dev@gmail.com>
2021-11-28 16:18:40 +0100

    feat: Autofocus todo input, hide todo list and footer when todos are empty, hide clear completed if there are no completed todos

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -83,79 +83,83 @@
         >
       {/if}
 
+      <!-- svelte-ignore a11y-autofocus -->
       <input
         bind:value={todo}
         type="text"
         class="new-todo"
         placeholder="What needs to be done?"
+        autofocus
       />
     </form>
 
-    <ul class="todo-list">
-      {#each filteredTodos as todo (todo.id)}
-        <li class:completed={todo.completed} class:editing class="todo">
-          <div class="view">
-            <input
-              bind:checked={todo.completed}
-              type="checkbox"
-              class="toggle"
-              class:completed={todo.completed}
-            />
-            <label on:dblclick={() => (editing = true)} for="todo">
-              {todo.text}
-            </label>
-            <button on:click={() => removeTodo(todo.id)} class="remove" />
-          </div>
+    {#if todosAmount}
+      <ul class="todo-list">
+        {#each filteredTodos as todo (todo.id)}
+          <li class:completed={todo.completed} class:editing class="todo">
+            <div class="view">
+              <input
+                bind:checked={todo.completed}
+                type="checkbox"
+                class="toggle"
+                class:completed={todo.completed}
+              />
+              <label on:dblclick={() => (editing = true)} for="todo">
+                {todo.text}
+              </label>
+              <button on:click={() => removeTodo(todo.id)} class="remove" />
+            </div>
 
-          {#if editing}
-            <input
-              class="edit"
-              on:input={() => console.log('input')}
-              on:blur={() => console.log('blur')}
-              value={todo.text}
-              type="text"
-            />
-          {/if}
-        </li>
-      {/each}
-    </ul>
+            {#if editing}
+              <input
+                class="edit"
+                on:input={() => console.log('input')}
+                on:blur={() => console.log('blur')}
+                value={todo.text}
+                type="text"
+              />
+            {/if}
+          </li>
+        {/each}
+      </ul>
 
-    <footer>
-      <span class="todo-count">
-        {uncompleted}
-        {uncompleted === 1 ? 'item' : 'items'} left
-      </span>
-      <div class="filters">
-        <button
-          on:click={() => (filter = 'all')}
-          class:selected={filter === 'all'}
-          class="filter"
-        >
-          All
-        </button>
-        <button
-          on:click={() => (filter = 'active')}
-          class:selected={filter === 'active'}
-          class="filter"
-        >
-          Active
-        </button>
-        <button
-          on:click={() => (filter = 'completed')}
-          class:selected={filter === 'completed'}
-          class="filter"
-        >
-          Completed
-        </button>
-      </div>
+      <footer>
+        <span class="todo-count">
+          {uncompleted}
+          {uncompleted === 1 ? 'item' : 'items'} left
+        </span>
+        <div class="filters">
+          <button
+            on:click={() => (filter = 'all')}
+            class:selected={filter === 'all'}
+            class="filter"
+          >
+            All
+          </button>
+          <button
+            on:click={() => (filter = 'active')}
+            class:selected={filter === 'active'}
+            class="filter"
+          >
+            Active
+          </button>
+          <button
+            on:click={() => (filter = 'completed')}
+            class:selected={filter === 'completed'}
+            class="filter"
+          >
+            Completed
+          </button>
+        </div>
 
-      <button
-        on:click={clearCompleted}
-        class:hidden={completed === 0}
-        class="clear-completed"
-      >
-        Clear completed
-      </button>
-    </footer>
+        <button
+          on:click={clearCompleted}
+          class:hidden={completed === 0}
+          class="clear-completed"
+        >
+          Clear completed
+        </button>
+      </footer>
+    {/if}
   </section>
 </main>
