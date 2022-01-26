48a72303c7c48283bc88cb8673c0d1ae12eb65eb
mattcroat <mattcroat.dev@gmail.com>
2022-01-26 14:04:23 +0100

    chore: Update test files to use TypeScript extension

--- src/components/tests/Todo.test.ts
+++ src/components/tests/Todo.test.ts
@@ -4,8 +4,8 @@ import Todo from '../Todo.svelte'
 
 // todo: should display todo item
 // todo: able to edit todo item
-// todo: check todo item
-// todo: uncheck todo item
+// todo: check todo item as completed
+// todo: uncheck todo item as incomplete
 // todo: remove todo
 // todo: striketrough completed todo
 
@@ -13,12 +13,13 @@ test('should display todo item', () => {
   let todo = { id: 1, text: 'Todo', completed: false }
 
   let props = {
-    duration: 0,
     editTodo: jest.fn(),
     removeTodo: jest.fn(),
     completeTodo: jest.fn(),
+    duration: 0,
   }
 
   render(Todo, { todo, ...props })
+
   expect(screen.getByText(/todo/i)).toBeInTheDocument()
 })
